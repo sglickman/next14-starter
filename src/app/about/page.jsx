f7dc1443db240faffe131dfc5d8377c497e305af
Seth <seth@ba.io>
2024-01-04 00:31:24 -0500

    1:21:14 in

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,30 @@ import Image from "next/image"
 
 const AboutPage = () => {
   return(
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>Blah blah blah</h1>
+        <p className={styles.desc}>
+          Let&apos;s get this done
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill />
+        <Image className={styles.img} src="/about.png" alt="" fill />
       </div>
     </div>
     )
